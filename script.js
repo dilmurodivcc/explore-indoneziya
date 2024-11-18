2e69c2610fffda0266773246cfda3d0b0575fe40
dilmurodivcc <zufarovamohire32.gmail.com>
2024-11-18 10:08:20 +0500

    add

--- script.js
+++ script.js
@@ -94,18 +94,46 @@ close.addEventListener("click", () => {
   document.body.style.overflowY = "auto";
 });
 
+AOS.init();
+AOS.init({
+  duration: 1800,
+});
+const customCursor = document.querySelector(".custom-cursor");
 
+document.addEventListener("mousemove", (e) => {
+  const smoke = document.createElement("div");
+  smoke.classList.add("tutun");
 
+  smoke.style.left = `${e.pageX}px`;
+  smoke.style.top = `${e.pageY}px`;
 
+  document.body.appendChild(smoke);
 
+  setTimeout(() => {
+    smoke.style.opacity = "0";
+  }, 100);
 
+  setTimeout(() => {
+    smoke.remove();
+  }, 550);
+});
+let backToTopBtn = document.getElementById("backToTopBtn");
 
+window.onscroll = function () {
+  scrollFunction();
+};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
 
-
-
-
-
-
-
-
+backToTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
